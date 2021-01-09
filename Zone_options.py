74d229a287defc3bf5f6e09f2aa3b6e5a6793813
Michael Bell <56834461+Mtb-3@users.noreply.github.com>
2021-01-09 19:18:59 +0100

    Add files via upload

---
+++ Zone_options.py
@@ -0,0 +1,193 @@
+OPTIONS = [
+#Act 1 Zones
+"Cavern of Anger",
+"Cavern of Anger 2",
+"Cavern of Wrath",
+"Cavern of Wrath 2",
+"Coast",
+"Coast 2",
+"Flooded Depths",
+"Flooded Depths 2",
+"Lower Prison",
+"Lower Prison 2",
+"Lower Prison 3",
+"Lower Prison 4",
+"Mud Flats",
+"Prisoner's Gate",
+"Ship Graveyard",
+"Ship Graveyard 2",
+"Ship Graveyard 3",
+"Submerged Passage 1" ,
+"Submerged Passage 2",
+"Submerged Passage 3",
+"Upper Prison",
+"Upper Prison 2",
+"Upper Prison 3",
+
+#Act 2 Zones
+"Caverns",
+"Caverns 2",
+"Caverns 3",
+"Chamber of Sins 1",
+"Chamber of Sins 1_2",
+"Chamber of Sins 1_3",
+"Chamber of Sins 2",
+"Vaal Ruins",
+"Vaal Ruins 2",
+"Vaal Ruins 3",
+"Wetlands",
+"Wetlands 2",
+"Weaver's Chambers",
+"Weaver's Chambers 2",
+
+#Act 3 Zones
+
+"Crematorium",
+"Crematorium 2",
+"Crematorium 3",
+"Docks",
+"Docks 2",
+"Docks 3",
+"Ebony Barracks",
+"Ebony Barracks 2",
+"Imperial Gardens",
+"Lunaris Temple 1",
+"Lunaris Temple 2_1",
+"Lunaris Temple 2_2",
+"Marketplace",
+"Marketplace 2",
+"Slums",
+"Slums 2",
+"Solaris Temple 1",
+"Solaris Temple 1_2",
+"Solaris Temple 2_1",
+"Solaris Temple 2_2",
+
+
+#Act 4 Zones
+
+"Ascent",
+"Belly of the Beast 1",
+"Belly of the Beast 1_2",
+"Belly of the Beast 2_1",
+"Belly of the Beast 2_2",
+"Crystal Veins",
+"Crystal Veins 2",
+"Dried Lake",
+"Dried Lake 2",
+"Harvest",
+"Kaom's Dream",
+"Kaom's Stronghold",
+"Mines 1",
+"Mines 2_1",
+"Mines 2_2",
+
+#Act 5 Zones
+"Chamber of Innocence",
+"Control Blocks",
+"Oriath Square",
+"Ossuary",
+"Reliquary",
+"Ruined Square",
+"Slave Pens",
+"Templar Courts",
+"Torched Courts",
+
+#Act 6 Zones
+
+"Brine King's Reef",
+"Brine King's Reef 2",
+"Karui Fortress",
+"Mud_Flats",
+"Mud_Flats 2",
+"Ridge",
+"Ridge 2",
+"The Cavern of Anger",
+"The Coast",
+"The Coast 2",
+"The Lower Prison",
+"The Lower Prison 2",
+"The Lower Prison 3",
+"The Prisoner's Gate",
+"The Prisoner's Gate 2",
+"The Riverways",
+
+#Act 7 Zones
+
+"The Chamber of Sins 1",
+"The Chamber of Sins 1_2",
+"The Chamber of Sins 1_3",
+"The Chamber of Sins 2",
+"The Crossroads",
+"The Crypt",
+"The Crypt 2",
+"The Crypt 3",
+"The Crypt 2_1",
+"The Crypt 2_2",
+"The Crypt 2_3",
+"The Den",
+"The Den 2",
+"The Vaal City",
+
+#Act 8 Zones
+
+"Bath House",
+"Doedre's Cesspool",
+"Doedre's Cesspool 2",
+"Lunaris Concourse",
+"The Grain Gate",
+"The Lunaris Temple 1",
+"The Lunaris Temple 1_2",
+"The Lunaris Temple 2_1",
+"The Lunaris Temple 2_2",
+"The Quay",
+"The Solaris Temple 1",
+"The Solaris Temple 2_1",
+"The Solaris Temple 2_2",
+"Toxic Conduits",
+
+
+#Act 9 Zones
+
+"Oasis",
+"Oasis 2",
+"Quarry",
+"Refinery",
+"The Belly of the Beast 1",
+"The Belly of the Beast 2",
+"The Belly of the Beast 3",
+"The Descent",
+"The Foothills",
+"Tunnel",
+"Tunnel 2",
+"Tunnel 3",
+"Vastiri Desert",
+
+#Act 10 Zones
+
+"Desecrated Chambers",
+"The Control Blocks",
+"The Ossuary",
+"The Ravaged Square",
+"The Reliquary",
+"The Torched Courts",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
